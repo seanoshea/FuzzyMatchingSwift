e0745ac903bfda7f9682b88519ec457ec16692d1
Sean O'Shea <oshea.ie@gmail.com>
2020-04-29 21:45:37 -0400

    #79 - Cleanup. (#80)
    * #79 - Cleanup.
    
    * #79 Upgrade.
    
    * #79 - Less storage.
    
    Co-authored-by: seanoshea <sean.oshea@amwell.com>

--- Example/FuzzyMatchingSwift_Example/AppDelegate.swift
+++ Example/FuzzyMatchingSwift_Example/AppDelegate.swift
@@ -22,8 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
+  func applicationDidFinishLaunching(_ application: UIApplication) {
+
   }
 }
